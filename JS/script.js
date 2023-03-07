59ba5de676027eeaf2b039b672eb2e410b91226c
MachadONLY <rm551570@fiap.com.br>
2023-03-07 11:49:10 -0300

    script.js adicionado ao html

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+//este é um comentario de linha 
+
+/**
+ * É um comentario 
+ * de
+ * Bloco
+ */
+
+console.log("OLa Mundo!")
+console.error("Bom dia")
+console.warn("Será")
+console.info("Vai ser...")
